10b54cb5c2ea9a3eabf841439e495cf851f97c09
GunroarCannon <gunroarcannon@gmail.com>
2025-09-02 16:40:19 +0100

    j

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ const transporter = nodemailer.createTransport({
 
 app.get("/health", (req, res) =>{
     res.send("Server is doing pretty okay if you ask me.");
-    
 })
+
 app.post("/demand", async (req, res) => {
     try {
 
