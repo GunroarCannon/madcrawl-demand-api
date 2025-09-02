103536713646aa78c20fb9af39803a2eb7dba0b3
GunroarCannon <gunroarcannon@gmail.com>
2025-09-02 16:42:26 +0100

    port

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ app.post("/demand", async (req, res) => {
 
 })
 
-app.listen(3000, ()=>{
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Server started on port ${process.env.PORT || 3000}`);
 
     runMongoConnection();
 })
